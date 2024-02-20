d15c29979c3a3411a516ff4fc594c57d183b3001
grow-xd <gupta.rg.rishabh@gmail.com>
2024-02-21 02:44:10 +0530

    added logic for getting the commits

--- src/extension.ts
+++ src/extension.ts
@@ -4,22 +4,45 @@ import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+// export function activate(context: vscode.ExtensionContext) {
+
+// 	// Use the console to output diagnostic information (console.log) and errors (console.error)
+// 	// This line of code will only be executed once when your extension is activated
+// 	console.log('Congratulations, your extension "commit-summarizer" is now active!');
+
+// 	// The command has been defined in the package.json file
+// 	// Now provide the implementation of the command with registerCommand
+// 	// The commandId parameter must match the command field in package.json
+// 	let disposable = vscode.commands.registerCommand('commit-summarizer.helloWorld', () => {
+// 		// The code you place here will be executed every time your command is executed
+// 		// Display a message box to the user
+// 		vscode.window.showInformationMessage('Hello World from Commit Summarizer!');
+// 	});
+
+// 	context.subscriptions.push(disposable);
+// }
+
 export function activate(context: vscode.ExtensionContext) {
+    const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "commit-summarizer" is now active!');
+    if (workspaceFolder) {
+        const gitExtension = vscode.extensions.getExtension('vscode.git');
+        if (gitExtension) {
+            const git = gitExtension.exports.getAPI(1);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('commit-summarizer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Commit Summarizer!');
-	});
+            const repository = git.repositories.find((repo: { rootUri: { fsPath: string; }; }) =>
+                repo.rootUri?.fsPath === workspaceFolder.uri.fsPath
+            );
 
-	context.subscriptions.push(disposable);
+            if (repository) {
+                const commits = repository.state.logEntries;
+
+                commits.forEach((commit: { message: any; }) => {
+                    console.log(commit.message);
+                });
+            }
+        }
+    }
 }
 
 // This method is called when your extension is deactivated
